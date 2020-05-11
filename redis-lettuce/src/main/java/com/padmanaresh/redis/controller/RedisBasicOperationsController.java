968a2a2369499202e1a5e7fe8831492e044a826b
Veeru <veerag@menlo-technologies.com>
2020-05-11 19:50:06 +0530

    added ops

--- redis-lettuce/src/main/java/com/padmanaresh/redis/controller/RedisBasicOperationsController.java
+++ redis-lettuce/src/main/java/com/padmanaresh/redis/controller/RedisBasicOperationsController.java
@@ -9,7 +9,11 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 public class RedisBasicOperationsController {
@@ -23,16 +27,48 @@ public class RedisBasicOperationsController {
     @Resource(name="stringRedisTemplate")
     private ValueOperations<String, String> opsForValue;
 
-    @GetMapping("setSimpleData/{key}/{value}")
+    /**
+     * Input
+     * setSimpleData/key1/value1
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    @GetMapping("set/{key}/{value}")
     public Set<String> setSimpleData(@PathVariable String key , @PathVariable String value){
         opsForValue.set(key,value);
         return stringRedisTemplate.keys("*");
     }
 
-    @PostMapping("setJsonData")
-    public Person setJsonData(@RequestBody Person person){
+    /**
+     * Input
+     * {"id":"123","name":"padma naresh","gender":"Male","age":32}
+     * @param person
+     * @return
+     */
+    @PostMapping("setJson")
+    public Person setJson(@RequestBody Person person){
         redisTemplate.opsForValue().set(person.getId(),person);
         return (Person)redisTemplate.opsForValue().get(person.getId());
     }
+
+    /**
+     * Input
+     * [{"id":"123","name":"padma naresh","gender":"Male","age":32}
+     * ,{"id":"124","name":"Punyasloka","gender":"Male","age":27}
+     * ]
+     *
+     * @param person
+     * @return
+     */
+    @PostMapping("multiSet")
+    public List<Person> multiSet(@RequestBody List<Person> person){
+        Map<String, Person> multipleData = person.stream().collect(Collectors.toMap(Person::getId, Function.identity()));
+        List<String> multiKeys = person.stream().map(Person::getId).collect(Collectors.toList());
+        redisTemplate.opsForValue().multiSet(multipleData);
+        return (List<Person>)redisTemplate.opsForValue().multiGet(multiKeys);
+    }
+
 
 }
--- redis-lettuce/src/main/java/com/padmanaresh/redis/controller/RedisListOperationsController.java
+++ redis-lettuce/src/main/java/com/padmanaresh/redis/controller/RedisListOperationsController.java
@@ -1,14 +1,62 @@
 package com.padmanaresh.redis.controller;
 
+import com.padmanaresh.redis.data.Person;
 import org.springframework.data.redis.core.ListOperations;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 public class RedisListOperationsController {
     // inject the template as ListOperations
-    @Resource(name="stringRedisTemplate")
-    private ListOperations<String, String> listOps;
+    @Resource(name="redisTemplate")
+    private ListOperations<Object, Object> listOps;
     // listOps.leftPush(userId, url.toExternalForm());
 
+    /**
+     * Input
+     * [{"id":"123","name":"padma naresh","gender":"Male","age":32}
+     * ,{"id":"124","name":"Punyasloka","gender":"Male","age":27}
+     * ]
+     *
+     * @param
+     * @return
+     */
+    @PostMapping("leftPush/{key}")
+    public List<Object> leftPush(@PathVariable String key,  @RequestBody List<Person> persons){
+        persons.forEach( person -> {
+            listOps.leftPush(key,person);
+        });
+       return  listOps.range(key, 0, -1);
+    }
+
+    /**
+     * Input
+     * [{"id":"0","name":"John Doe","gender":"Male","age":11},
+     *  {"id":"1","name":"John Doe1","gender":"Male","age":12},
+     *  {"id":"2","name":"John Doe2","gender":"Male","age":13}]
+     *
+     * @param
+     * @return
+     */
+    @PostMapping("leftPushAll/{key}")
+    public List<Object> leftPushAll(@PathVariable String key,  @RequestBody List<Person> persons){
+        listOps.leftPushAll(key,persons);
+        return  listOps.range(key, 0, -1);
+    }
+
+
+    /**
+     * Input
+     * @param
+     * @return
+     */
+    @GetMapping("rightPop/{key}")
+    public Object rightPopAndLeftPush(@PathVariable String key){
+        return  listOps.rightPop(key);
+    }
 }
